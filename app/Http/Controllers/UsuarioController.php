c629b57b2ad93dd87c45cb8b158ae449a4756ec3
sthef <sthef.ldo@gmail.com>
2026-01-19 13:32:55 -0400

    prueba para mostrar las peliculas por estatus

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -22,7 +22,13 @@ class UsuarioController extends Controller
     public function show($id)
     {
         $usuario=User::findOrFail($id);
-        return view('panel.usuarios.show', compact('usuario'));
+
+        $favoritasPosEstatus = $usuario->estatusPeliculas()
+        ->where('favorita', true)
+        ->with('pelicula')
+        ->orderBy('estatus')
+        ->get();
+        return view('panel.usuarios.show', compact('usuario', 'favoritasPosEstatus'));
     }
     public function edit($id){}
     public function update(Request $request, $id)
@@ -31,3 +37,45 @@ class UsuarioController extends Controller
     }
     public function destroy($id){}
 }
+
+
+
+
+/* public function show($id)
+{
+    $usuario = User::findOrFail($id);
+
+    // favoritas, ordenadas por estatus
+    $favoritasPorEstatus = $usuario->estatusPeliculas()
+        ->where('favorita', true)
+        ->with('pelicula')
+        ->orderBy('estatus') // 'abandonada', 'por_ver', 'sin_estatus', 'vista'
+        ->get();
+
+    // si quieres, separadas por cada estatus:
+    $favoritasVista = $usuario->estatusPeliculas()
+        ->where('favorita', true)
+        ->where('estatus', 'vista')
+        ->with('pelicula')
+        ->get();
+
+    $favoritasPorVer = $usuario->estatusPeliculas()
+        ->where('favorita', true)
+        ->where('estatus', 'por_ver')
+        ->with('pelicula')
+        ->get();
+
+    $favoritasAbandonada = $usuario->estatusPeliculas()
+        ->where('favorita', true)
+        ->where('estatus', 'abandonada')
+        ->with('pelicula')
+        ->get();
+
+    return view('panel.usuarios.show', compact(
+        'usuario',
+        'favoritasPorEstatus',
+        'favoritasVista',
+        'favoritasPorVer',
+        'favoritasAbandonada',
+    ));
+} */
--- resources/views/panel/usuarios/show.blade.php
+++ resources/views/panel/usuarios/show.blade.php
@@ -9,5 +9,19 @@
     <h1>Perfil de Usuario</h1>
     <p><strong>Nombre:</strong> {{ $usuario->name }}</p>
     <p><strong>Email:</strong> {{ $usuario->email }}</p>
+
+
+    <h2>Películas Favoritas</h2>
+    @if($favoritasPosEstatus->isEmpty())
+        <p>Este usuario no tiene películas favoritas.</p>
+    @else
+    <ul>
+        @foreach ($favoritasPosEstatus as $favorita)
+          <li>{{$favorita->pelicula->nombre}} -> {{$favorita->estatus}}</li>  
+        @endforeach
+    </ul>
+    @endif
+
+    
 </body>
 </html>
